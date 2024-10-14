5ba9b29e0875f4e76aad1814a3bafdb5a39976bb
vishvaditya <vishvadityaluhach@gmail.com>
2024-10-14 13:34:47 -0700

    updated main for number of system calls

--- user/test_info_run.c
+++ user/test_info_run.c
@@ -9,5 +9,7 @@ int main(){
     info(0);
     printf("--1. Number of processes running --\n");
     printf("---------%d---------\n", info(1));
+    printf("--2. Number of system calls by the current process --\n");
+    printf("---------%d---------\n", info(2));
     exit(0);
 }
