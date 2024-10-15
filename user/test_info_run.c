02bbb7dea9e9b697dfefca121c18d5eb965ee3ee
vishvaditya <vishvadityaluhach@gmail.com>
2024-10-14 22:04:10 -0700

    added test info run

--- user/test_info_run.c
+++ user/test_info_run.c
@@ -7,9 +7,15 @@
 int main(){
     printf("Testing custom system call\n");
     info(0);
-    printf("--1. Number of processes running --\n");
-    printf("---------%d---------\n", info(1));
+    // printf("--1. Number of processes running --\n");
+    // printf("---------%d---------\n", info(1));
     printf("--2. Number of system calls by the current process --\n");
     printf("---------%d---------\n", info(2));
+    for(int i = 0; i < 5; i++)
+    {
+    write(1, ".", 1);
+    }
+  
+    printf("\nSystem call count after writes: %d\n", info(2));
     exit(0);
 }
